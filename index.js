122b9eccb2fb4b35189e0fe3b46aef8b847636d3
Ilyas Mammadov <ilyas.mammadov.96@gmail.com>
2018-04-18 22:12:55 +0300

    son durum

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var app = express();
 var path = require('path');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-global.kullanicilar = [];
+var mysql = require('mysql');
 
 app.use('/public', express.static(path.join(__dirname, 'public'))); 
 app.use('/views', express.static(path.join(__dirname, 'views'))); 
@@ -16,11 +15,31 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+global.kullanicilar = [];
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "AZNp5415893"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
 io.on('connection', function (socket) {
-    console.log(socket.connected);
-    console.log("clietnden bağlantı var");
-    socket.on('event', function (data) {
-        global.kullanicilar.push(data);
-        console.log(global.kullanicilar);
+    var userinfo;
+    socket.on('yeni kullanici', function (data) {
+        
+    });
+
+    socket.on('disconnect', function(data){
+        if(userinfo in global.kullanicilar){
+            global.kullanicilar[global.kullanicilar.indexOf(userinfo)].onlinedurum = false;
+            socket.broadcast.emit('kullanicilistele', global.kullanicilar);
+            console.log('bir kullanıcı offline oldu');
+            console.log(global.kullanicilar);
+        }
     });
 });
--- public/js/js.js
+++ public/js/js.js
@@ -1,12 +1,67 @@
 $(document).ready(function(){
+    var socket = io.connect('http://localhost:1111');
 
     $('#giris').click(function(){
+        $('.giris').show();
+        $('.main').hide();
+        $('.kaydol').hide();
+    });
+
+    $('#kaydol').click(function(){
+        $('.kaydol').show();
+        $('.main').hide();
+        $('.giris').hide();
+    });
+
+    $('#register-form').submit(function(e){
+        e.preventDefault();
+
+        var username = $('#rusername').val();
+        socket.emit('register', username);
+        $('.kaydol').hide();
+        $('.main').show();
+    });
+
+    $('#login-form').submit(function(e){
+        e.preventDefault();
+
+        var username = $('#rusername').val();
+        socket.emit('login', username, function(data){
+            if(data){
+                $('.giris').hide();
+                $('#messenger').show();
+            }
+            else{
+                alert('Böyle bir kullanıcı yok!\nVeya kullanıcı adını yanlış girdiniz!');
+            }
+        });
+    });
+
+
+
+
+
+
+    $('#login-form').submit(function (e){
+        e.preventDefault();
         $.getJSON('https://ipapi.co/json/', function(data) {
             var ip = data.ip;
             var veri = {nick: $('#nick').val(), ip: ip };
-            var socket = io.connect('http://localhost:1111');
-            socket.emit('event', veri);
-            window.location = "http://localhost:1111/views/messenger.html";
+            $('.container').children().remove();
+            $('.container').append('<div class="col-sm-3 left-sidebar"><ul class="list-group userlist"></ul></div><div class="col-sm-9 content"></div>');
+            socket.emit('yeni kullanici', veri);
         });
+    });
+
+    socket.on('kullanicilistele', function(users){
+        $('.userlist').children().remove();
+        for(var i in users){
+            if(users[i].onlinedurum){
+                $('.userlist').append('<li class="list-group-item user-list-item">' + users[i].nick + '<div class="online"></div></li>');
+            }
+            else{
+                $('.userlist').append('<li class="list-group-item user-list-item">' + users[i].nick + '<div class="offline"></div></li>');
+            }
+        }
     });
 });
