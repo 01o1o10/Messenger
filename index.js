1fcac43d69149d55ecf49ae6a8495165afd42b93
Ilyas Mammadov <ilyas.mammadov.96@gmail.com>
2018-04-20 03:48:10 +0300

    asdsadasdgf

--- index.js
+++ index.js
@@ -15,27 +15,33 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-global.kullanicilar = [];
+global.kullanicilar = {};
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "AZNp5415893"
-  });
+});
   
-  con.connect(function(err) {
+con.connect(function(err) {
     if (err) throw err;
     con.query("use messenger");
     console.log("DB Connect successful!");
-  });
+});
+
+function query(sql, callback){
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        callback(result);
+    });
+}
 
 io.on('connection', function (socket) {
-
-    socket.on('register', function(data, callback){
-        con.query("SELECT * FROM users WHERE username = '" + data + "'", function (err, result) {
+    socket.on('register', function(user1, callback){
+        con.query("SELECT * FROM users WHERE username = '" + user1 + "'", function (err, result) {
             if (err) throw err;
-            if(result.length == 0){
-                var sql = "insert into users values('" + data + "', 0);";
+            if(result.length == 0){//böyle bir kullanıcı yoksa
+                var sql = "insert into users values('" + user1 + "', 0);";
                 con.query(sql, function (err, result) {
                     if (err) throw err;
                     console.log("1 record inserted");
@@ -43,7 +49,7 @@ io.on('connection', function (socket) {
                 });
             }
             else{
-                console.log("Bu Kullanici ismi doludur.");
+                console.log("Zaten bu isme sahip bir kullanıcı var.");
                 callback(false);
             }
             
@@ -52,13 +58,14 @@ io.on('connection', function (socket) {
         
     });
 
-    socket.on('login', function(data, callback){
-        socket.username = data;
-        con.query("SELECT username FROM users WHERE username='" + data + "'", function (err, result, fields) {
+    socket.on('login', function(user, callback){
+        con.query("SELECT username FROM users WHERE username='" + user + "'", function (err, result, fields) {
             if (err) throw err;
 
             if(result.length == 1){
-                con.query("UPDATE users SET onlinedurum = 1 WHERE username='" + data + "';");
+                socket.username = user;
+                global.kullanicilar[user] = socket;
+                con.query("UPDATE users SET onlinedurum = 1 WHERE username='" + user + "';");
                 callback(true);
             }
             else{
@@ -68,27 +75,57 @@ io.on('connection', function (socket) {
     });
 
     socket.on('set users', function(setUsers){
-        con.query("SELECT * FROM users", function (err, result, fields) {
-            if (err) throw err;
-            setUsers(result);
+        query("SELECT * FROM users", setUsers);
+    });
+
+    socket.on('user selected', function(user1, user2, callback){
+        query("select * from izinler where user1='" + user1 + "' and user2='" + user2 + "';", function(result){
+            if(result.length == 0){
+                callback('<div class="izin-istegi1"><h5>Bu kullanıcı ile çetleşmek için izniniz bulunmamaktadır.</br>İzin isteğinde bulunmak ister misiniz?</h5><div class="btn-group"><button class="btn btn-primary btn-lg button" type="button" id="evet">Evet</button><button class="btn btn-primary btn-lg button" type="button" id="hayir">Hayır</button></div></div>');
+            }
+            else if(result[0].istek == 1){
+                callback('<div class="izin-istegi1">Gönderdiğiniz izin isteği beklemede...</br></div>');
+            }
+            else if(result[0].izin == 0){
+                callback('<div class="izin-istegi1">Bu kullanıcı sizinle çetleşmek istemiyor!</br></div>')
+            }
+            else{
+                callback(false);
+            }
         });
     });
 
-    //bağlantıı olduğunda bunlar çalışacak.
-
-    /*var userinfo;
-    socket.on('yeni kullanici', function (data) {
-        
+    socket.on('izin istegi', function(user1, user2){
+        query("insert into izinler values('" + user1 + "','" + user2 + "', 0, 1);", function(result){});
+        global.kullanicilar[user2].emit('izin istegi', user1);
     });
 
-    socket.on('disconnect', function(data){
-        if(userinfo in global.kullanicilar){
-            global.kullanicilar[global.kullanicilar.indexOf(userinfo)].onlinedurum = false;
-            socket.broadcast.emit('kullanicilistele', global.kullanicilar);
-            console.log('bir kullanıcı offline oldu');
-            console.log(global.kullanicilar);
-        }
-    });*/
+    socket.on('message', function(message, user1, user2){
+        /*con.query("SELECT * FROM izinler WHERE (user1='" + user1 + "' and user2='" + user2 + "') and izin=" + 1 + ";", function (err, result, fields) {
+            if (err) throw err;
+            if(result.length == 1){*/
+                con.query("INSERT INTO messages(user1, user2, message, okundu) values('" + user1 + "','" + user2 +"','" + message + "', 0);", function (err, result, fields) {
+                    if (err) throw err;
+                    console.log("mesaj eklendi");
+                });
+            /*}
+        });*/
+    });
+
+    socket.on('get perm reqs', function(user2, callback){
+        query("select * from izinler where user2='" + user2 + "' and istek=1;", callback);
+    });
+
+    socket.on('set perm', function(user1, user2, perm){
+        console.log('set perm aldı');
+        console.log(user1 + " " + user2);
+        query("update izinler set istek=0, izin=" + perm + " where user1='" + user1 + "' and user2='" + user2 + "';", function(result){
+            console.log('izinler güncellendi');
+            console.log(result);
+            global.kullanicilar[user1].emit('perm res', perm, user1);
+            console.log('perm res gönderilmiş olmalı');
+        });
+    });
 
     socket.on('disconnect', function(){
         con.query("UPDATE users SET onlinedurum = 0 WHERE username='" + socket.username + "';");
--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     var socket = io.connect('http://localhost:1111');
+    var username;
+    var selectedUsername;
+
+    ///SELECTORS
 
     $('#giris').click(function(){
         $('.giris').show();
@@ -32,7 +36,8 @@ $(document).ready(function(){
     $('#login-form').submit(function(e){
         e.preventDefault();
 
-        var username = $('#lusername').val();
+        username = $('#lusername').val();
+        socket.emit('get perm reqs',  username, setPerReqs);
         socket.emit('login', username, function(data, ){
             if(data){
                 $('.header').hide();
@@ -41,46 +46,107 @@ $(document).ready(function(){
                 socket.emit('set users', setUsers);
             }
             else{
-                alert('Böyle bir kullanıcı yok!\nVeya kullanıcı adını yanlış girdiniz!');
+                $('.giris').append('<h5 style="color: red; text-align: center;">Kullanıcı adı geçersiz!</br>Kontrol ederek tekrar deneyiniz.</h5>');
             }
         });
     });
 
-    function setUsers(users){
-        $('#users-tbody').children().remove();
-        for(var i in users){
-            if(users[i].onlinedurum){
-                $('#users-tbody').append('<tr><td>' + users[i].username +'<div class="online"></div></td></tr>');
+    $(document).on('click', 'tr', function(){///user selection
+        $('.content').children().remove();
+        selectedUsername = $(this).children().children().first().next().html();
+        socket.emit('user selected', username, selectedUsername, function(message){
+            if(message){
+                $('.content').append(message);
             }
             else{
-                $('#users-tbody').append('<tr><td>' + users[i].username +'<div class="offline"></div></td></tr>');
+                $('#message-form').show();
             }
-        }
-    }
+            $('.izin-istegi1 #evet').click(function(){
+                $('.content').children().remove();
+                socket.emit('izin istegi', username, selectedUsername);
+                $('.content').append('<div class="izin-istegi1">İstek gönderildi!</br></div>');
+            });
+            $('.izin-istegi1 #hayir').click(function(){
+                $('.content').children().remove();
+            });
+        });
+    });
+    
+    $(document).on('click', '#kabulet', function(){
+        var user = $(this).attr('username');
+        $(this).parent().prev().remove();
+        $(this).parent().remove();
+        alert('kabul edildi');
+        setPerm(1, user);
+    });
+
+    $(document).on('click', '#reddet', function(){
+        var user = $(this).attr('username');
+        $(this).parent().prev().remove();
+        $(this).parent().remove();
+        setPerm(0, user);
+    });
+
+
+    $('#message-submit').click(function(){
+        var message = $('#message').val();
+        socket.emit('message', message, username, selectedUsername);
+        $('.content').append('<div class="message"><p style="font-size: 1em; float: right;">' + message +'</p></div>');
+    });
+
+    ///EVENTS
 
     socket.on('refresh users', function(users){
         setUsers(users);
     });
-
-
-
-
-/*
-
-    $('#login-form').submit(function (e){
-        e.preventDefault();
-        $.getJSON('https://ipapi.co/json/', function(data) {
-            var ip = data.ip;
-            var veri = {nick: $('#nick').val(), ip: ip };
-            $('.container').children().remove();
-            $('.container').append('');
-            socket.emit('yeni kullanici', veri);
-        });
+    
+    socket.on('izin istegi', function(user1){
+        $('.dropdown-menu').append('<p><strong style="color: blue;">' + user1 + '</strong> sizinle çetleşmek istiyor.<p><div class="btn-group"><button class="btn btn-primary btn-sm button" type="button" id="kabulet" username="' + user1 + '">Kabulet</button><button class="btn btn-primary btn-sm button" type="button" id="reddet" username="' + user1 +  '">Reddet</button></div></div><hr>');
+        //ses çıkar
     });
 
-    socket.on('kullanicilistele', function(users){
-        $('.userlist').children().remove();
-        for(var i in users){
+    socket.on('perm res', function(perm, user2){
+        alert('perm resi aaldı');
+        if(selectedUsername == user2){
+            alert('hala aynı kullanıcının mesajları açık');
+            alert('izin sonucu' + perm);
+            if(perm){
+                alert('contenti temizleyip mesajlaşma kısmını göstermesi lazım');
+                $('.content').children().remove();
+                $('#message-form').show();
+            }
+            else{
+                alert('content e red cevabını vermesi lazım')
+                $('.content').append('<div class="receiver-message"><p style="font-size: 1em; color: red;">İstek reddedildi!</p></div>');
+            }
         }
-    });*/
+    });
+
+    ///FUNCTİONS
+    function setPerm(perm, user){
+        console.log(username + " " + user);
+        alert('set perm i gönderecek');
+        socket.emit('set perm', user, username, perm);
+    }
+
+    function setUsers(users){
+        $('#users-tbody').children().remove();
+        for(var i in users){
+            if(users[i].username != username){
+                if(users[i].onlinedurum){
+                    $('#users-tbody').append('<tr><td><div class="online"></div><p>' + users[i].username +'</p></td></tr>');
+                }
+                else{
+                    $('#users-tbody').append('<tr><td><div class="offline"></div><p>' + users[i].username +'</p></td></tr>');
+                }
+            }
+        }
+    }
+
+    function setPerReqs(perReqs){
+        $('.dropdown-menu').children().remove();
+        for(var i in perReqs){
+            $('.dropdown-menu').append('<p><strong style="color: blue;">' + perReqs[i].user1 + '</strong> sizinle çetleşmek istiyor.<p><div class="btn-group"><button class="btn btn-primary btn-sm button" type="button" id="kabulet" username="' + perReqs[i].user1 + '">Kabulet</button><button class="btn btn-primary btn-sm button" type="button" id="reddet" username="' + perReqs[i].user1 +  '">Reddet</button></div></div><hr>');
+        }
+    }
 });
